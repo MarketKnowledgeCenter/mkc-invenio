d36cbd4d6e0272203c994abe98901ea32d1237ca
Coni <coni@MacBook-Pro-de-Luciana.local>
2024-11-03 16:50:02 -0300

    try locking pipfile

--- site/mkc_sandbox/views.py
+++ site/mkc_sandbox/views.py
@@ -8,7 +8,7 @@ from flask import Blueprint
 def create_blueprint(app):
     """Register blueprint routes on app."""
     blueprint = Blueprint(
-        "mkc_sandbox",
+        "mkc_invenio",
         __name__,
         template_folder="./templates",
     )
